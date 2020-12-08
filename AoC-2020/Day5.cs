3d4c33ee614a193834faff878217104466c97369
Anthony Cole <anthony.cole@gmail.com>
2020-12-08 00:20:38 +0000

    Advent of Code: Day 5-6

---
+++ AoC-2020/Day5.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace AoC_2020
+{
+    [TestFixture]
+    public class Day5
+    {
+        private string rawInput { get; set; }
+
+        private List<BoardingPass> boardingPasses = new List<BoardingPass>();
+
+        [SetUp]
+        public void Setup()
+        {
+            rawInput = System.IO.File.ReadAllText(Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"..\..",
+                "Input",
+                "Day5.txt"));
+
+
+            string pattern = @"([FB]{7})([LR]{3})";
+            // Create a Regex  
+            Regex rg = new Regex(pattern);
+
+            MatchCollection matchedPolicies = rg.Matches(rawInput);
+
+            boardingPasses = matchedPolicies.Cast<Match>().Select(x => new BoardingPass()
+            {
+                Row = Convert.ToInt32(x.Groups[1].Value.Replace("F", "0").Replace("B", "1").Trim(), 2),
+                Column = Convert.ToInt32(x.Groups[2].Value.Replace("L", "0").Replace("R", "1").Trim(), 2)
+            }).ToList();
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Console.WriteLine(boardingPasses.Max(x => x.SeatId));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            boardingPasses.Sort((pass1, pass2) => pass1.SeatId.CompareTo(pass2.SeatId));
+
+            var firstNumber = boardingPasses.First().SeatId;
+
+            var lastNumber = boardingPasses.Last().SeatId;
+
+            var range = Enumerable.Range(firstNumber, lastNumber - firstNumber);
+
+            var missingNumbers = range.Except(boardingPasses.Select(x => x.SeatId));
+
+            Console.WriteLine(missingNumbers.FirstOrDefault());
+        }
+    }
+
+    public class BoardingPass
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int SeatId => (Row * 8) + Column;
+    }
+}
---
+++ AoC-2020/Day6.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace AoC_2020
+{
+    [TestFixture]
+    public class Day6
+    {
+        private string rawInput { get; set; }
+        private string[] groupAnswers { get; set; }
+        private string[] individualAnswers { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            rawInput = System.IO.File.ReadAllText(Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"..\..",
+                "Input",
+                "Day6.txt"));
+
+            rawInput = rawInput.Replace("\r\n\r\n", "\0");
+            groupAnswers = rawInput.Replace("\r\n", "").Split('\0');
+            individualAnswers = rawInput.Split('\0');
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var distinctItems = groupAnswers.Aggregate(0, (acc, value) =>
+            {
+                acc += value.Distinct().ToArray().Count();
+                return acc;
+            });
+
+            Console.WriteLine(distinctItems);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Console.WriteLine(individualAnswers);
+            Dictionary<char, int> answerDictionary = new Dictionary<char, int>();
+
+            var countOfAnswers = 0;
+
+            foreach (var answer in individualAnswers)
+            {
+                answerDictionary = new Dictionary<char, int>();
+                var allAnswers = answer.Replace("\r", "").Split('\n');
+
+                if (allAnswers.Length == 1)
+                {
+                    // Single person, all answers accepted
+                    countOfAnswers += allAnswers[0].Length;
+                    continue;
+                }
+
+                // if a party of people check when answers agree
+                foreach (var individual in allAnswers)
+                {
+                    for (int i = 0; i < individual.Length; i++)
+                    {
+                        if (answerDictionary.ContainsKey(individual[i]))
+                        {
+                            answerDictionary[individual[i]] += 1;
+                        }
+                        else
+                        {
+                            answerDictionary.Add(individual[i], 1);
+                        }
+                    }
+                }
+
+                countOfAnswers += answerDictionary.Count(x => x.Value == allAnswers.Length);
+            }
+            Console.WriteLine(countOfAnswers);
+        }
+    }
+}
